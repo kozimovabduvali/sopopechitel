8285c7a0fddc8246109ef0c8663c9f7d6c771da7
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-05-11 13:39:35 +0500

    git

--- src/components/Pages/Home/OurGoal.jsx
+++ src/components/Pages/Home/OurGoal.jsx
@@ -4,15 +4,15 @@ function OurGoal() {
   return (
     <div>
       <h3 className='font-extrabold md:text-xl uppercase'>Наша цель</h3>
-      <div className='flex justify-between flex-col md:flex-row gap-x-5 md:gap-x-10 lg:gap-x-19 gap-y-10 mt-6 md:mt-8 lg:mt-10'>
-        <p className='font-medium md:text-xl'>
+      <div className='flex flex-col md:flex-row gap-x-5 md:gap-x-10 lg:gap-x-19 gap-y-10 mt-6 md:mt-8 lg:mt-10'>
+        <p className='lg:max-w-90 leading-[100%] font-medium md:text-xl'>
           Стремимся дать каждому ребёнку шанс вырасти благополучным взрослым
         </p>
         {/* Right */}
-        <div className='w-full max-w-211.5 space-y-8 md:space-y-15 lg:space-y-18'>
+        <div className='w-full max-w-211.5 space-y-8 md:space-y-15 lg:space-y-18 ml-auto'>
           <div>
-            <h2 className='font-semibold text-xl md:text-[2rem]/[100%] lg:text-[2.5rem]/[100%]'>
-              Формируем сообщество взрослых, готовых к осознанной и устойчивой поддержке детей
+            <h2 className='font-semibold text-xl md:text-[2rem]  lg:text-[2.5rem] leading-[100%]'>
+            Формируем сообщество взрослых, готовых к&nbsp;осознанной и&nbsp;устойчивой поддержке детей
             </h2>
             <button className="border border-dark rounded-xl inline-flex items-center transition duration-200 hover:bg-dark hover:text-white  gap-3 h-9 px-5 mt-6">
               <span className="font-medium">Как вы можете помочь</span>
@@ -30,8 +30,8 @@ function OurGoal() {
             </button>
           </div>
           <div>
-            <h2 className='font-semibold text-xl md:text-[2rem]/[100%] lg:text-[2.5rem]/[100%]'>
-              Создаём масштабируемую модель наставничества с методологией ИИ-сопровождением
+            <h2 className='font-semibold text-xl md:text-[2rem] lg:text-[2.5rem] leading-[100%]'>
+            Создаём масштабируемую модель наставничества с&nbsp;методологиейи&nbsp;ИИ-сопровождением
             </h2>
             <button className="border border-dark rounded-xl inline-flex items-center transition duration-200 hover:bg-dark hover:text-white  gap-3 h-9 px-5 mt-6">
               <span className="font-medium">Как внедряем ИИ модель</span>
@@ -50,7 +50,7 @@ function OurGoal() {
           </div>
         </div>
       </div>
-      <div className='max-w-195 mx-auto pt-35 md:pt-30'>
+      <div className='max-w-200 mx-auto pt-35 md:pt-30'>
         <h2 className='font-semibold text-center text-[2rem]/[100%] md:text-5xl lg:text-[64px]/[100%]'>
           Мы даём ребёнку удочку, а не рыбу, чтобы он выловил нужный и эффективный путь для становления успешным взрослым
         </h2>
